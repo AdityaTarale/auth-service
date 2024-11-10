cb3a16fbd91686b5dc7a6d456d41a3bd66094994
AdityaTarale <tarale.adi@gmail.com>
2024-11-10 16:30:37 +0530

    test lint-staged 4

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,5 @@
 function welcome(name: string) {
-    console.log(name)
-    const user = {
-        name: 'Aditya',
-    }
-    const fname = user.name
-
-    return name + fname
+    return name
 }
 
 welcome('Aditya')
