9ca0116ccf845ae530da8675ab3b2e52572db148
AdityaTarale <tarale.adi@gmail.com>
2024-11-29 21:46:12 +0530

    add logger

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,10 @@
 import { config } from 'dotenv'
 config()
 
-const { PORT, NODE_ENV } = process.env
+const { SERVICE_NAME, PORT, NODE_ENV } = process.env
 
 export const Config = {
+    SERVICE_NAME,
     PORT,
     NODE_ENV,
 }
---
+++ src/config/logger.ts
@@ -0,0 +1,39 @@
+import winston from 'winston'
+import { Config } from '.'
+
+const logger = winston.createLogger({
+    level: 'info', // Root/Fallback level
+    defaultMeta: { service: Config.SERVICE_NAME },
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            dirname: 'logs',
+            filename: 'combined.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV === 'test',
+        }),
+        new winston.transports.File({
+            level: 'error',
+            dirname: 'logs',
+            filename: 'error.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV === 'test',
+        }),
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV === 'test',
+        }),
+    ],
+})
+
+export default logger
