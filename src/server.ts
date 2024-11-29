32d6d50cecddaa447b447c1422b26e6a9aad6aa7
AdityaTarale <tarale.adi@gmail.com>
2024-11-29 21:21:13 +0530

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-function welcome(name: string) {
-    console.log('Welcome to the node-template' + name)
+import app from './app'
+import { Config } from './config'
+
+const startServer = () => {
+    const PORT = Config.PORT
+    try {
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-welcome('Aditya')
+startServer()
